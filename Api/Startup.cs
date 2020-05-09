1181ee62fcfd0fbbf697cce9969b4cf5a0f0fc85
gribbib <j.git@motschi.email>
2020-05-09 15:41:33 +0200

    Add chat hub to webapi project

--- Api/Startup.cs
+++ Api/Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using SignalRChat.Hubs;
 
 namespace FirstNewSteps.Api
 {
@@ -26,6 +27,7 @@ namespace FirstNewSteps.Api
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddSignalR();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -45,6 +47,7 @@ namespace FirstNewSteps.Api
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
+                endpoints.MapHub<ChatHub>("/chatHub");
             });
         }
     }
